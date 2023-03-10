89833b1fdd24d5add2f679f05bdda1c67bdda9c0
LukeVdKraats <lukejekraats@gmail.com>
2023-03-10 11:30:51 +0100

    Script modified

--- VRAlgebra/Assets/Rotate.cs
+++ VRAlgebra/Assets/Rotate.cs
@@ -5,70 +5,82 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
 
-    public float rotationSpeed = 0.2f;
-    static public Quaternion nearestState = Quaternion.Euler(0, 0, 0);
-    //public allRotations Quaternion = new Quaternion[24];
-
-    /*void start()
-    {
-        for (int i = 0; i <= 3; i++)
-            allRotations[i] = Quaternion.Euler(i * 90, 0, 0);
-        for (int i = 0; i <= 3; i++)
-            allRotations[i + 4] = Quaternion.Euler(0, i * 90, 0);
-        for (int i = 0; i <= 3; i++)
-            allRotations[i + 8] = Quaternion.Euler(0, 0, 90 * i);
-    }*/
-
-    void Update()
-    {
-
-    }
-
-    //Rotate the object with the mouse
-    void OnMouseDrag()
-    {
-        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
-        // select the axis by which you want to rotate the GameObject
-        transform.RotateAround(Vector3.down, XaxisRotation);
-        transform.RotateAround(Vector3.right, YaxisRotation);
-    }
+	public float rotationSpeed = 0.2f;
+	//static public Quaternion nearestState = Quaternion.Euler(0, 0, 0);
+	static public Quaternion[] allRotations = new Quaternion[24];
 
 
-    //Go back to original state
-    void OnMouseUp()
-    {
-        var angle = Quaternion.Angle(transform.rotation, nearestState);
-        StartCoroutine(PerformRotation(nearestState));
-    }
+	void start()
+	{
+		for (int i = 0; i <= 3; i++)
+			allRotations[i] = Quaternion.AngleAxis(i * 90, Vector3(0, 1, 0));
+		for (int i = 0; i <= 2; i++)
+			allRotations[i + 4] = Quaternion.AngleAxis(90 + i * 90, Vector3(1, 0, 0));
+		for (int i = 0; i <= 3; i++)
+			allRotations[i + 7] = Quaternion.AngleAxis(90 + i * 90, Vector3(0, 0, 1));
+		for (int i = 0; i <= 2; i++)
+			allRotations[i + 10] = Quaternion.AngleAxis(180, Vector3((i + 1) % 3 - 1, (i - 1) % 3 - 1, i % 3 - 1));
+		for (int i = 0; i <= 2; i++)
+			allRotations[i + 13] = Quaternion.AngleAxis(180, Vector3((i - 1) % 3 - 1, (i + 1) % 3 - 1, i % 3 - 1));
+		for (int i = 0; i <= 1; i += 1)
+		{
+			allRotations[4 * i + 15] = Quaternion.angleAxis(120 + 120 * i, Vector3(-1, -1, 1));
+			allRotations[4 * i + 17] = Quaternion.angleAxis(120 + 120 * i, Vector3(-1, 1, 1));
+			allRotations[4 * i + 18] = Quaternion.angleAxis(120 + 120 * i, Vector3(1, -1, 1));
+			allRotations[4 * i + 19] = Quaternion.angleAxis(120 + 120 * i, Vector3(1, 1, 1));
+		}
+	}
 
-    IEnumerator PerformRotation(Quaternion targetRotation)
-    {
-        float progress = 0f;
-        float speed = 0.3f;
+	void update()
+	{
 
-        while (progress < 1f)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, progress);
-            progress += Time.deltaTime * speed;
+	}
 
-            if (progress <= 1f)
-            {
-                yield return null;
-            }
-        }
-    }
+	//Rotate the object with the mouse
+	void OnMouseDrag()
+	{
+		float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+		float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+		//select the axis by which you want to rotate the GameObject
+		transform.RotateAround(Vector3.down, XaxisRotation);
+		transform.RotateAround(Vector3.right, YaxisRotation);
+	}
 
 
+	//Go back to original state
+	void OnMouseUp()
+	{
+		var angle = Quaternion.Angle(transform.rotation, nearestState);
+		StartCoroutine(PerformRotation(ClosestRotation(allRotations)));
+	}
 
-    /*static Quaternion ClosestRotation(Quaternion[] allRotations)
-    {
+	IEnumerator PerformRotation(Quaternion targetRotation)
+	{
+		float progress = 0f;
+		float speed = 0.3f;
+
+		while (progress < 1f)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, progress);
+			progress += Time.deltaTime * speed;
+
+			if (progress <= 1f)
+			{
+				yield return null;
+			}
+		}
+	}
+
+	static Quaternion ClosestRotation(Quaternion[] rotations)
+	{
 		Quaternion closest;
-		foreach(Quaternion v in allRotations)
-        {
+		foreach (Quaternion v in rotations)
+		{
 			if (Quaternion.Angle(transform.rotation, v) < smallest)
 				closest = v;
 		}
+
 		return closest;
-    }*/
+	}
 }
+
