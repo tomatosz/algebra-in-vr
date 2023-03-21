0a3bb4673235d7f1a79f31e5f63b59e500dd4914
Tomatosz <sjerries@hotmail.com>
2023-03-21 17:31:26 +0100

    Add Franca's changes

---
+++ VRAlgebra/Assets/Scripts/Rotate2.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate2 : MonoBehaviour
+{
+
+    public float rotationSpeed = 0.2f;
+    static public Quaternion[] allRotations = GenerateRotations();
+    public Material Redtransp;
+    public Material Greentransp;
+    public Material Transparant;
+    public bool Tetraok=true;
+    private int[] teraederrotationsindex = { 0, 2, 5, 8, 16, 17, 18, 19, 20, 21, 22, 23 };
+
+
+
+    void start()
+    {
+
+    }
+
+    void update()
+    {
+    }
+
+    //Rotate the object with the mouse
+    void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+        //select the axis by which you want to rotate the GameObject
+        transform.RotateAround(Vector3.down, XaxisRotation);
+        transform.RotateAround(Vector3.right, YaxisRotation);
+        this.GetComponent<MeshRenderer>().material = Transparant;
+    }
+
+    //Go back to original state
+    void OnMouseUp()
+    {
+        Quaternion closest = ClosestRotation(allRotations);
+        if(Quaternion.Angle(transform.rotation, closest)<20)
+        {
+            StartCoroutine(PerformRotation(closest));
+            this.GetComponent<MeshRenderer>().material = Greentransp;
+        }
+        else
+            this.GetComponent<MeshRenderer>().material = Redtransp;
+
+    }
+
+    //Perform the rotation to a target rotation
+    IEnumerator PerformRotation(Quaternion targetRotation)
+    {
+        float progress = 0f;
+        float speed = 0.3f;
+
+        while (progress < 1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, progress);
+            progress += Time.deltaTime * speed;
+
+            if (progress <= 1f)
+            {
+                yield return null;
+            }
+        }
+    }
+
+
+    //Generate all rotations
+    static Quaternion[] GenerateRotations()
+    {
+        Quaternion[] allRotations = new Quaternion[24];
+        //
+        for (int i = 0; i <= 3; i++)
+            allRotations[i] = Quaternion.AngleAxis(i * 90, new Vector3(0, 1, 0));
+        for (int i = 0; i <= 2; i++)
+            allRotations[i + 4] = Quaternion.AngleAxis(90 + i * 90, new Vector3(1, 0, 0));
+        for (int i = 0; i <= 2; i++)
+            allRotations[i + 7] = Quaternion.AngleAxis(90 + i * 90, new Vector3(0, 0, 1));
+        //
+        for (int i = 0; i <= 2; i++)
+            allRotations[i + 10] = Quaternion.AngleAxis(180, new Vector3((i + 1) % 3 - 1, (i + 2) % 3 - 1, i % 3 - 1));
+        for (int i = 0; i <= 2; i++)
+            allRotations[i + 13] = Quaternion.AngleAxis(180, new Vector3(((i + 2) % 3 - 1)* ((i + 2) % 3 - 1), ((i + 1) % 3 - 1)* ((i + 1) % 3 - 1), (i % 3 - 1)* (i % 3 - 1)));
+        //
+        for (int i = 0; i <= 1; i += 1)
+        {
+            allRotations[4 * i + 16] = Quaternion.AngleAxis(120 + 120 * i, new Vector3(-1, -1, 1));
+            allRotations[4 * i + 17] = Quaternion.AngleAxis(120 + 120 * i, new Vector3(-1, 1, 1));
+            allRotations[4 * i + 18] = Quaternion.AngleAxis(120 + 120 * i, new Vector3(1, -1, 1));
+            allRotations[4 * i + 19] = Quaternion.AngleAxis(120 + 120 * i, new Vector3(1, 1, 1));
+        }
+
+        return allRotations;
+    }
+
+    //Finded the nearest Rotation
+    Quaternion ClosestRotation(Quaternion[] rotations)
+    {
+        int indexclosest=0;
+        
+        Quaternion closest = rotations[0];
+        for (int i = 0; i < rotations.Length; i++)
+        {
+            if (Quaternion.Angle(transform.rotation, rotations[i]) < Quaternion.Angle(transform.rotation, closest))
+            {
+                closest = rotations[i];
+                indexclosest = i;
+            }
+        }
+        this.Tetraok = false;
+        for(int j=0;j<12;j++)
+        {
+            if (indexclosest == teraederrotationsindex[j])
+            {
+                this.Tetraok = true;
+                
+            }
+            
+        }
+        return closest;
+    }
+}
---
+++ VRAlgebra/Assets/Scripts/Tetrahedron.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Tetrahedron : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    public Rotate2 cube;
+    
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateTetrahedron();
+        Updatemesh();
+        if (cube.Tetraok == true)
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else
+            this.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void Update()
+    {
+        
+        if (cube.Tetraok == true)
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else
+            this.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void CreateTetrahedron()
+    {
+        Vector3 p0 = new Vector3(1, 1, 1);
+        Vector3 p1 = new Vector3(-1, 1, -1);
+        Vector3 p2 = new Vector3(1, -1, -1);
+        Vector3 p3 = new Vector3(-1, -1, 1);
+
+        vertices = new Vector3[]
+        {
+            p0,
+            p1,
+            p2,
+            p3
+        };
+
+        triangles = new int[]{
+            0,2,1,
+            0,1,3,
+            0,3,2,
+            1,2,3
+            
+        };
+    }
+
+    void Updatemesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.Optimize();
+        mesh.RecalculateNormals();
+        
+    }
+}
+
