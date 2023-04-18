b1a915bf202104c6121701077c054dcc8bb2b110
Franca <39587464+Franca3@users.noreply.github.com>
2023-04-18 12:41:53 +0200

    vr rotate 2 working

--- VRAlgebra/Assets/Scripts/Rotate2.cs
+++ VRAlgebra/Assets/Scripts/Rotate2.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Rotate2 : MonoBehaviour
 {
-    public float rotationSpeed = 20f;
+    public float rotationSpeed = 50f;
     static public Quaternion[] allRotations = GenerateRotations();
     public Material Redtransp;
     public Material Greentransp;
@@ -15,14 +16,30 @@ public class Rotate2 : MonoBehaviour
     public bool Tetraok=true;
     private int[] teraederrotationsindex = { 0, 2, 5, 8, 16, 17, 18, 19, 20, 21, 22, 23 };
     public InputActionProperty rightHandVelocity;
+    public GameObject PlayerPosition;
     public Vector3 velocity { get; private set; } = new Vector3(3f,3f,3f);
 
-    private bool isTriggerPressed =true;
+    private bool isTriggerPressed =false;
     
     void Update()
     {
         velocity = rightHandVelocity.action.ReadValue<Vector3>();
-        Debug.Log(velocity.ToString());
+        if (isTriggerPressed == true)
+        {
+            float YaxisRotation=0f;
+            float XaxisRotation=0f;
+            Debug.Log(velocity.ToString());
+            if(Math.Abs(velocity.x)>0.1*Math.Abs(velocity.y))
+               XaxisRotation = velocity.x * rotationSpeed;
+            if (Math.Abs(velocity.y) > 0.1 * Math.Abs(velocity.x))
+               YaxisRotation  = velocity.y * rotationSpeed;
+            
+            //select the axis by which you want to rotate the GameObject
+            transform.RotateAround(Vector3.down, XaxisRotation);
+            transform.RotateAround(new Vector3(-(PlayerPosition.transform.position.z - this.transform.position.z), 0, PlayerPosition.transform.position.x - this.transform.position.x), YaxisRotation);
+            this.GetComponent<MeshRenderer>().material = Transparant;
+        }
+
     }
 
     //Rotate the object with the mouse
@@ -30,15 +47,7 @@ public class Rotate2 : MonoBehaviour
     {
                 
         isTriggerPressed = true;
-        if (isTriggerPressed == true)
-        {
-            float XaxisRotation = velocity.x * rotationSpeed;
-            float YaxisRotation = velocity.y * rotationSpeed;
-            //select the axis by which you want to rotate the GameObject
-            transform.RotateAround(Vector3.down, XaxisRotation);
-            transform.RotateAround(Vector3.right, YaxisRotation);
-            this.GetComponent<MeshRenderer>().material = Transparant;
-        }
+        
     }
 
     //Go back to original state
